7df6629328188c1ca081c54b478d795a6c7cf6ad
TeamDman <TeamDman9201@gmail.com>
2018-08-15 01:56:32 -0400

    Commit before moving to node

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -11,7 +11,6 @@ public class main {
 
 	public static void main(String[] args) {
 		client.getDispatcher().registerListener(MessageHandler.class);
-		client.getDispatcher().registerListener(main.class);
 		MessageHandler.read();
 	}
 }
